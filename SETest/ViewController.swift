a653dfafa1a3354fa0021336a34c69da567c6363
Ehsan Yazdan Panah <ehsan.pyk@gmail.com>
2017-10-19 19:03:09 +0100

    iOS version 1.0.2

--- SETest/ViewController.swift
+++ SETest/ViewController.swift
@@ -15,21 +15,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var Lat: UITextField!
     @IBOutlet weak var Lng: UITextField!
     @IBOutlet weak var MapVi: MKMapView!
+    @IBOutlet weak var MapView2: MKMapView!
     
     @IBOutlet weak var SLocarion: UIButton!
-    @IBOutlet weak var CLocation: UIButton!
     
     let manager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        CLocation.isEnabled = false
-        
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
-        //manager.startUpdatingLocation()
+        manager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,33 +36,42 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
     @IBAction func ClickedButton(_ sender: Any) {
-        self.view.endEditing(true);
-        CLocation.isEnabled = true
-        print("Button was pressed.")
-        let latText: String = Lat.text!;
-        let lngText: String = Lng.text!;
-        let latVal = Double(latText);
-        let lngVal = Double(lngText);
-        
-        let location = CLLocationCoordinate2D(latitude:latVal!,longitude: lngVal!);
-        let span = MKCoordinateSpanMake(0.05, 0.05);
-        let region = MKCoordinateRegion(center: location, span: span);
-        MapVi.setRegion(region, animated: true)
-        
-        // Add a pin here
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: latVal!, longitude: lngVal!)
-        MapVi.addAnnotation(annotation)
-        
-        textmessage.text = "Location of Latitude of " + latText + " and Longitude of " + lngText;
+        if (Lat.text?.isEmpty)! && (Lng.text?.isEmpty)!{
+            textmessage.text = "Enter a valid Latitude and Longitude..."
+        }
+        else{
+            self.view.endEditing(true);
+            print("Button was pressed.")
+            let latText: String = Lat.text!;
+            let lngText: String = Lng.text!;
+            let latVal = Double(latText);
+            let lngVal = Double(lngText);
+            
+            let location = CLLocationCoordinate2D(latitude:latVal!,longitude: lngVal!);
+            let span = MKCoordinateSpanMake(0.01, 0.01);
+            let region = MKCoordinateRegion(center: location, span: span);
+            MapVi.setRegion(region, animated: true)
+            
+            manager.stopUpdatingLocation()
+            
+            // Add a pin here
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: latVal!, longitude: lngVal!)
+            MapVi.addAnnotation(annotation)
+            
+            textmessage.text = "Location of Latitude of " + latText + " and Longitude of " + lngText;
+        }
     }
-    
     
     @IBAction func CurrentLoc(_ sender: Any) {
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         //manager.startUpdatingLocation()
+    }
+    
+    @IBAction func MyLocation(_ sender: UIButton) {
+        manager.startUpdatingLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -80,5 +87,4 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         textmessage.text = "Location of Latitude of " + String(location.coordinate.latitude) + " and Longitude of " + String(location.coordinate.longitude);
     }
-    
 }
